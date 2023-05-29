0ed14a38240a21722f21adf6583e2fbf5fa3f3ac
Seba-piantanida <sebastiano.piantanida@mail.polimi.it>
2023-05-30 01:17:00 +0200

    ri organizazione

--- Project_RTL.vhd
+++ Project_RTL.vhd
@@ -51,55 +51,61 @@ begin
         o_done <= '0';
         
         temp_z0 <= (others => '0');
-        temp_z0 <= (others => '0');  --inizializzo memoria uscite a 0
-        temp_z0 <= (others => '0');
-        temp_z0 <= (others => '0');
+        temp_z1 <= (others => '0');  --inizializzo memoria uscite a 0
+        temp_z2 <= (others => '0');
+        temp_z3 <= (others => '0');
 
         o_mem_we <= '0';
         o_mem_en <= '0';
 
         state <= s1;
 
+        out_data <= (others => '0');
+
         temp_addr <= (others => '0');
+
+        o_mem_addr <= (others => '0');
     
     elsif i_clk'event and i_clk = '1' then
         case state is
-            when s1 =>
+            when s1 =>                  --leggo primo bit porta di uscita
 
                 if i_start = '1' then
                     out_port(1) <= i_w;
                     state <= s2;
                 end if;
                 
-            when s2 =>
+            when s2 =>                  --leggo secondo bit porta di uscita
 
                 if i_start = '1' then 
                     out_port(0) <= i_w;
                     state <= s3;
                 end if;
 
-            when s3 =>
+            when s3 =>                  --leggo indirizzo di memoria
 
                 if i_start = '1' then
                     temp_addr <= temp_addr(14 downto 0) & i_w;
                 elsif i_start = '0' then
                     state <= s4;
+                    o_mem_en <= '1';
+                    o_mem_addr <= temp_addr;
                 end if;
 
-            when s4 =>
+            when s4 =>                  --abilito la memoria e inserisco l'indirizzo
 
                 o_mem_en <= '1';
                 o_mem_addr <= temp_addr;
                 state <= s5;
 
-            when s5 =>
+            when s5 =>                  --leggo i dati dalla memoria
 
                 out_data <= i_mem_data;
-                o_mem_en <= '0';
                 state <= s6;
+                o_mem_en <= '0';
 
-            when s6 =>
-
+            when s6 =>                  --salvo i dati sulla "memoria del uscita"
+                
                 case out_port is
                     when "00" =>
                         temp_z0 <= out_data;
@@ -107,14 +113,13 @@ begin
                         temp_z1 <= out_data;
                     when "10" =>
                         temp_z2 <= out_data;
-                    when others  =>
+                    when others =>
                         temp_z3 <= out_data;
-                     
                 end case;
-
+                
                 state <= s7;
 
-            when s7 =>
+            when s7 =>              --mostro le uscite in output e porto done a 1
 
                 o_done <= '1';
                 o_z0  <= temp_z0;
@@ -124,7 +129,7 @@ begin
 
                 state <= s8;
 
-            when s8 =>
+            when s8 =>              -- stato di reset
                 
                 o_z0  <= (others => '0');
                 o_z1  <= (others => '0');
@@ -132,11 +137,16 @@ begin
                 o_z3  <= (others => '0');
 
                 o_done <= '0';
+                
+                o_mem_we <= '0';
+                o_mem_en <= '0';
+        
+                out_data <= (others => '0');
+                temp_addr <= (others => '0');
                 state <= s1;
 
-            when others =>
-
-                state <=s0;
+            when s0 => -- sato di idle finche il sistema non riceve il primo rst resta qui 
+                state <= s0;
 
             end case;
           end if;
